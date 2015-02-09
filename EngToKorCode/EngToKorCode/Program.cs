fae918394c38f56277d9f947e97d9fb9578b6824
Jeong Won Cheol <sturdyegg@gmail.com>
2015-02-09 12:19:40 +0900

    영어를 한글로 변환

---
+++ EngToKorCode/EngToKorCode/Program.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngToKorCode
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Program pg = new Program();
+			Trans t = new Trans();
+			Console.WriteLine( t.englishToKorean("dksssud") );
+		}
+	}
+
+	/////////////////////////////////////////////////////////////////
+	//영문 타이핑을 한글로 변환
+	//
+	//입력 문자열을 하나씩 가져온다.
+	//ㄴ 길이 추출후 인덱스로 하나씩 자름 substring
+	//가져온 문자열이 영문인지 확인한다.
+	//ㄴ 유니코드 범위로 확인하고 범위를 벗어날 경우 그냥 넘긴다.
+	//영문일 경우 한글 유니코드 인덱스로 변환한다.
+	//종성 뒤에 모음이 오는 경우
+	//이 전 글자의 종성을 0으로 하고 그 종성을 이번 글자의 초성으로 가져온다.
+	//
+
+	//input
+	//dkssud 안녕
+	//dksud 아녕
+	//rmfTpdy? 글쎄요?
+	//qork rhvmek. 배가 고프다. 
+	//한글이 tjRdu 있어요. 한글이 섞여 있어요.
+	
+
+	class Trans
+	{
+		
+		string KoreaText;
+		Int32 initialCode;
+		Int32 medialCode;
+		Int32 finalCode;
+		int textLength;
+
+		Dictionary<string, string> no_alphabet_list;
+		void Add_String(string value)
+		{
+			no_alphabet_list.Add(value, value);
+		}
+
+		public Trans()
+		{
+			//이거 스트링으로 바꿔서 for문 처리
+			no_alphabet_list = new Dictionary<string, string>();
+			Add_String("1");
+			Add_String("2");
+			Add_String("3");
+			Add_String("4");
+			Add_String("5");
+			Add_String("6");
+			Add_String("7");
+			Add_String("8");
+			Add_String("9");
+			Add_String("0");
+			Add_String("!");
+			Add_String("@");
+			Add_String("#");
+			Add_String("$");
+			Add_String("%");
+			Add_String("^");
+			Add_String("&");
+			Add_String("*");
+			Add_String("(");
+			Add_String(")");
+			Add_String("?");
+			Add_String(" ");
+			Add_String(".");
+			Add_String(",");
+			Add_String(";");
+			Add_String("-");
+			Add_String("+");
+			Add_String("=");
+		}
+		public string englishToKorean(string text)
+		{
+	
+			KoreaText = "";
+			textLength = text.Length;
+			//들어온 영문 텍스트를 한글자씩 잘라요.
+			for (var idx = 0; idx < textLength; idx++)
+			{
+				if (space_check(text.Substring(idx, 1)))
+				{
+					continue;
+				}
+				// 초성 코드 추출
+				initialCode = getCode("initial", text.Substring(idx, 1));
+
+
+				/**
+				  * 현재 문자와 다음 문자를 합한 문자열의 중성 코드 추출
+				  * ㅞ ( np ) 또는 ㄼ ( fq ) 같은 두개의 문자가 들어가는 것을 체크하기 위함
+				  */
+				if (initialCode == -1)
+				{
+					if (space_check(text.Substring(idx, 1)))
+					{
+						continue;
+					}
+				}
+				else
+				{
+					idx++; // 다음 문자로.
+				}
+
+				Int32 tempMedialCode = -1;
+				if (idx + 2 <= textLength)
+					tempMedialCode = getCode("medial", text.Substring(idx, 2));
+
+				// 코드 값이 있을 경우
+				if (tempMedialCode != -1)
+				{
+					// 코드 값을 저장하고 인덱스가 다다음 문자열을 가르키게 한다.
+					medialCode = tempMedialCode;
+					idx += 2;
+				}
+				else // 코드값이 없을 경우 하나의 문자에 대한 중성 코드 추출
+				{
+					if (idx + 1 <= textLength)
+						medialCode = getCode("medial", text.Substring(idx, 1));
+					if (medialCode == -1)
+					{
+						if (space_check(text.Substring(idx, 1)))
+						{
+							continue;
+						}
+					}
+
+					idx++; // 다음 문자로.
+
+				}
+				// 현재 문자와 다음 문자를 합한 문자열의 종성 코드 추출
+				Int32 tempFinalCode = -1;
+				if (idx + 2 <= textLength)
+					tempFinalCode = getCode("final", text.Substring(idx, 2));
+				// 코드 값이 있을 경우
+				if (tempFinalCode != -1)
+				{
+					// 코드 값을 저장한다.
+					finalCode = tempFinalCode;
+					// 그 다음의 중성 문자에 대한 코드를 추출한다.
+					if (idx + 3 <= textLength)
+						tempMedialCode = getCode("medial", text.Substring(idx + 2, 1));
+					else
+						tempMedialCode = -1;
+					// 코드 값이 있을 경우
+					if (tempMedialCode != -1)
+					{
+						// 종성 코드 값을 저장한다.
+						if (idx + 1 <= textLength)
+							finalCode = getCode("final", text.Substring(idx, 1));
+					}
+					else
+					{
+						idx++;
+					}
+				}
+				else // 코드 값이 없을 경우
+				{
+					// 그 다음의 중성 문자에 대한 코드 추출
+					if (idx + 2 <= textLength)
+						tempMedialCode = getCode("medial", text.Substring(idx + 1, 1));
+					else
+						tempMedialCode = -1;
+					// 그 다음에 중성 문자가 존재할 경우
+					if (tempMedialCode != -1)
+					{
+						// 종성 문자는 없음.
+						finalCode = 0;
+						idx--;
+					}
+					else
+					{
+						// 종성 문자 추출
+						if (idx + 1 <= textLength)
+							finalCode = getCode("final", text.Substring(idx, 1));
+						else
+							finalCode = -1;
+						if (finalCode == -1)
+						{
+							if ((idx - 1) + 1 <= textLength)
+							{
+								if (space_check(text.Substring(idx - 1, 1)))
+								{
+									continue;
+								}
+							}
+
+							finalCode = 0;
+
+						}
+					}
+				}
+				// 추출한 초성 문자 코드, 중성 문자 코드, 종성 문자 코드를 합한 후 변환하여 출력
+
+				String result = new String(new char[] { Convert.ToChar(0xAC00 + initialCode + medialCode + finalCode) });
+				KoreaText += result;
+			}
+			return KoreaText;
+		}
+
+		// 메소드명이 잘못됐음.
+		// 공백이나 특수문자의 경우 그대로 패스!
+		bool space_check(string val)
+		{
+			if (no_alphabet_list.ContainsKey(val))
+			{
+				KoreaText += val;
+				return true;
+			}
+			return false;
+		}
+
+		/**
+		  * 해당 문자에 따른 코드를 추출한다.
+		  * @param type 초성 : chosung, 중성 : jungsung, 종성 : jongsung 구분
+		  * @param char 해당 문자
+		  */
+		Int32 returnCode;
+		bool isFind; // 문자를 찾았는지 체크 변수
+		// 초성
+		string initial = "rRseEfaqQtTdwWczxvg";
+		// 중성
+		string[] medial = new string[] { "k", "o", "i", "O", "j", "p", "u", "P", "h", "hk", "ho", "hl", "y", "n", "nj", "np", "nl", "b", "m", "ml", "l" };
+		// 종성
+		string[] final = new string[] { "r", "R", "rt", "s", "sw", "sg", "e", "f", "fr", "fa", "fq", "ft", "fx", "fv", "fg", "a", "q", "qt", "t", "T", "d", "w", "c", "z", "x", "v", "g" };
+		Int32 getCode(string type, string val)
+		{
+
+			isFind = false;
+
+			if (type == "initial")
+			{
+				returnCode = initial.IndexOf(val) * 21 * 28;
+				if (returnCode < 0)
+					isFind = false;
+				else
+					isFind = true;
+			}
+			else if (type == "medial")
+			{
+				for (var i = 0; i < medial.Length; i++)
+				{
+					if (medial[i] == val)
+					{
+						returnCode = i * 28;
+						isFind = true;
+						break;
+					}
+				}
+			}
+			else if (type == "final")
+			{
+				for (var i = 0; i < final.Length; i++)
+				{
+					if (final[i] == val)
+					{
+						returnCode = i + 1;
+						isFind = true;
+						break;
+					}
+				}
+			}
+			else
+			{
+				//alert("잘못된 타입입니다.");
+			}
+			if (isFind == false) returnCode = -1; // 값을 찾지 못했을 경우 -1 리턴
+			return returnCode;
+		}
+	}
+}
--- codeTest/codeTest/Program.cs
+++ codeTest/codeTest/Program.cs
@@ -1,8 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//프로그래밍 목적 정리
+//1. 빠르고 정확하게 띄울 수 있을 것
+//ㄴ 필드에 들어가면 한글로 자동 전환해준다.
+//ㄴ 최소의 클릭을 사용하도록 한다.
+//ㄴ 자동 완전 기능으로 오타를 보정한다.
+//ㄴ 영어로 쳐도 한글로 나오도록
+//ㄴ 단축 이름 호출 가능
+//ㄴ 장 단위로 불러오기
+
 
 namespace codeTest
 {
@@ -10,32 +20,118 @@ namespace codeTest
 	{
 		static void Main(string[] args)
 		{
-
-			Console.WriteLine("HelloWorld!");
+			//---------------------------------------------
 			//http://inobae.blog.me/90007564511
-
-            String initial = "ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ";
-			String medial = "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ";
-			String final = "ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ";
-            Console.WriteLine(initial.IndexOf("ㄴ"));
-			ushort uni = 0xAC00;
-            Console.WriteLine(Convert.ToChar(uni));
-
-            //초성 중성 종성 이 있는 한 글자 클래스를 만들고
-			
-			//문자열을 넣으면
-			//들어온 문자열을 초중성 클래스 배열로 반환한다.
+			//완성형 한글 유니코드 시작 AC00 = 가 ~ D7AF(실제 문자가 있는 것은 D79F)
+			//한글 자모 유니코드 시작 1100 = ㄱ ~ 11FF
+			//한글코드 위치 = 한글 시작위치 + (초성 인덱스 * 21 + 중성 인덱스) * 28 + 종성 인덱스
+			//초성길이 = 19, 중성길이 = 21, 종성길이 = 28(공백포함)
+			//ex) 안 = 0xAC00 + (11 * 21 + 0) * 28 + 3
+			//ex) 녕 = 0xAC00 + (2 * 21 + 6) * 28 + 21
 			//---------------------------------------------
 
-			//반환된 배열을 기존에 있는 리스트에 넣는다.
-			//리스트에서 정렬 후에 가장 근접한 단어를 반환한다.
+			String initial = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ";
+			String medial = "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ";
+			String final = " ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ";
+			Console.WriteLine(initial.IndexOf("ㄴ"));
+			Console.WriteLine(medial.IndexOf("ㅕ"));
+			Console.WriteLine(final.IndexOf("ㅇ"));
+			ushort uni = 0xAC00 + (2 * 21 + 6) * 28 + 21;
+			Console.WriteLine(Convert.ToChar(uni));
+
+			if (initial[0] == 0x1100)
+				Console.WriteLine("true");
+
+			//-------------------------------------------
+			String oldBibleString = @"창세기 출애굽기 레위기 민수기 신명기 여호수아 사사기 룻기 사무엘상 사무엘하 열왕기상 열왕하기 역대상 역대하 에스라 느헤미야 에스더 욥기 시편 잠언 전도서 아가 이사야 예레미야 예레미야애가 에스겔 다니엘 호세아 요엘 아모스 오바댜 요나 미가 나훔 하박국 스바냐 학개 스가랴 말라기";
+			String newBibleString = @"마태복음 마가복음 누가복음 요한복음 사도행전 로마서 고린도전서 고린도후서 갈라디아서 에베소서 빌립보서 골로새세 데살로니가전서 데살로니가후서 디모데전서 디모데후서 디도서 빌레몬서 히브리서 야고보서 베드로전서 베드로후서 요한일서 요한이서 요한삼서 유다서 요한계시록";
+			String bibleShortcutString = @"왕상 왕하";
+			ArrayList bibleList = new ArrayList();
+			String[] oldBibleList = oldBibleString.Split(' ');
+			String[] newBibleList = newBibleString.Split(' ');
+			String[] bibleShortcutList = bibleShortcutString.Split(' ');
+			foreach (String oldbible in oldBibleList)
+				bibleList.Add(oldbible);
+			foreach (String newbible in newBibleList)
+				bibleList.Add(newbible);
+			foreach (String bibleShortcut in bibleShortcutList)
+				bibleList.Add(bibleShortcut);
+
+			bibleList.Sort();
+
+			for (int i = 0; i < bibleList.Count; i++)
+			{
+				Console.WriteLine(i + " : " + bibleList[i]);
+			}
+
+			String keyword = "곤";
+			bibleList.Add(keyword);
+			bibleList.Sort();
+			int idx = bibleList.IndexOf(keyword);
+
+			Console.WriteLine("keyword : " + keyword);
+			Console.WriteLine("result : " + bibleList[idx + 1]);
+
+			Program pg = new Program();
+			Console.WriteLine("test parse : " + pg.DevideChar('ㄱ'));
+
+			//---------------------------------------------
+			//입력 경우의 수
+			//1.[한] 정확히 맞을 경우 - 통과
+			//2.[한] 앞은 맞았으나 뒤가 틀릴경우 - 통과
+			//3.[한] 중간이나 앞이 틀릴 경우 - 파서로 분석 후 일치율로 자동완성
+			//4.[한] 약자를 사용하고 정확히 맞는 경우 - 약자 등록 후 입력이 약자인지 판별 후에 원문에 연결
+			//5.[한] 약자를 사용하고 뒤가 틀릴 경우 - 4번 과정 후 통과
+			//6.[한] 약자를 사용하고 중간이나 앞이 틀릴 경우 - 3번 과정 후 4번
+			//7.[영] 정확히 맞을 경우 - 한글로 변환 후 일치율 확인
+		}
+
+		public void EngToKor()
+		{
+			//들어온 영어 입력을 파싱된 한글로 변환
+		}
+
+		public void SearchBible(String bible)
+		{
+			//들어온 값을 파서를 이용해 한글자모 파싱
+			//성경이름 파싱 리스트 불러옴
+			//불러온 리스트에서 파싱된 글자의 일치율을 확인
+			//가장 일치율이 높은 성경을 호출
+			//호출된 성경이 약자일 경우 원래 성경을 재호출
+		}
 
 
+		//한 글자를 자모로 나누어 반환
+		public String DevideChar(char ch)
+		{
+			int initial, medial, final;
+			ushort korStart = 0xAC00;
+			ushort temp = 0x0000;
+			temp = Convert.ToUInt16(ch);
+
+			//한글이 아닐 경우에 대한 처리 해야됨.
+
+			int code = temp - korStart;
+			initial = code / (21 * 28);
+			code = code % (21 * 28);
+			medial = code / 28;
+			code = code % 28;
+			final = code;
+
+			String result = initial + " " + medial + " " + final;
+			return result;
 		}
 	}
 
 	class ParseChar
 	{
-		int initial, medial, 
+		int initial, medial, final;
+
+		ParseChar()
+		{
+			initial = 0;
+			medial = 0;
+			final = 0;
+		}
 	}
 }
